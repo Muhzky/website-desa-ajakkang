64fae6e45254cf65bc171c523eee57eb745cf67b
Muhzky <qayza.celluler1@gmail.com>
2025-12-15 21:54:29 +0800

    hadehhhh

--- config/livewire.php
+++
@@ -1,9 +0,0 @@
-'temporary_file_upload' => [
-    'disk' => 'public',
-    'directory' => 'livewire-tmp',
-    'rules' => ['file', 'max:5120'],
-    'middleware' => null,
-    'preview_mimes' => [
-        'png', 'jpg', 'jpeg', 'webp',
-    ],
-],
